58949ffdc4f10e24d12d2432e14bae9ef04652c0
Alexey Gorbunov <alexey.gorbunov@devexpress.com>
2021-08-02 04:42:38 -0700

    DevExpress Collection View for .NET MAUI Preview 6

---
+++ CS/CollectionViewExample/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace CollectionViewExample
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	}
+}
