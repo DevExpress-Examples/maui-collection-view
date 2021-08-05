e4ca6c1388af161b34c7da8754f3ca3a71d86ce7
Alexey Gorbunov <alexey.gorbunov@devexpress.com>
2021-08-05 09:49:20 +0200

    view model

---
+++ CS/CollectionViewExample/ViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CollectionViewExample
+{
+    public class Contact
+    {
+        string name;
+        public string Name
+        {
+            get => this.name;
+            set
+            {
+                this.name = value;
+                if (Photo == null)
+                {
+                    string resourceName = value.Replace(" ", "").ToLower() + ".jpg";
+                    Photo = ImageSource.FromFile(resourceName);
+                }
+            }
+        }
+
+        public Contact(string name, string phone)
+        {
+            Name = name;
+            Phone = phone;
+        }
+        public ImageSource Photo { get; set; }
+        public string Phone { get; set; }
+    }
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public List<Contact> Data { get; }
+        public ViewModel()
+        {
+            Data = new List<Contact>() {
+                new Contact("Nancy Davolio", "(206) 555-9857"),
+                new Contact("Andrew Fuller", "(206) 555-9482"),
+                new Contact("Janet Leverling", "(206) 555-3412"),
+                new Contact("Margaret Peacock", "(206) 555-8122"),
+                new Contact("Steven Buchanan", "(71) 555-4848"),
+                new Contact("Michael Suyama", "(71) 555-7773"),
+                new Contact("Robert King", "(71) 555-5598"),
+                new Contact("Laura Callahan", "(206) 555-1189"),
+                new Contact("Anne Dodsworth", "(71) 555-4444"),
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
