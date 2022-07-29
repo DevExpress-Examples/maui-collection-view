d3b597af7c955644ab17218a2cf8b2f32cbedcd0
Alexey Gorbunov <alexey.gorbunov@devexpress.com>
2022-07-29 11:57:38 +0200

    edits

--- CS/CollectionViewSelection/MainPage.xaml.cs
+++ CS/CollectionViewSelection/MainPage.xaml.cs
@@ -1,7 +1,5 @@
 ﻿namespace CollectionViewSelection {
     public partial class MainPage : ContentPage {
-        int count = 0;
-
         public MainPage() {
             InitializeComponent();
         }
